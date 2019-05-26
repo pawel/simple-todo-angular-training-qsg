af705aa38e57c79aa8820d1c05f54caecba1b29f
pawel <pawel@users.noreply.github.com>
2019-05-26 10:52:56 +0200

    Stats component (#3)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {MainTodoComponent} from './components/todo/main-todo.component';
-import {AddTaskComponent} from './components/add-task/add-task.component';
-import {AddTaskRxComponent} from './components/add-task-rx/add-task-rx.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainTodoComponent } from './components/todo/main-todo.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AddTaskRxComponent } from './components/add-task-rx/add-task-rx.component';
 
 const routes: Routes = [
     { path: 'todo', component: MainTodoComponent },
     { path: 'add-task-rx', component: AddTaskRxComponent },
     { path: 'add-task', component: AddTaskComponent },
-    {path: '', redirectTo: '/todo', pathMatch: 'full'}
+    { path: '', redirectTo: '/todo', pathMatch: 'full' }
 ];
 
 @NgModule({
